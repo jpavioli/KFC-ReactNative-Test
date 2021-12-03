205230d834c21c0dc486e354b38a55764818d8ec
mParticle <support@mparticle.com>
2021-12-03 12:53:44 -0500

    First Commit

--- App.js
+++ App.js
@@ -1,12 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+// Import mParticle RN SDK
+import MParticle from 'react-native-mparticle'
+
+// Import RN ATT
+// https://github.com/mrousavy/react-native-tracking-transparency
+import {
+  getTrackingStatus,
+  requestTrackingPermission
+} from 'react-native-tracking-transparency';
+
+const dictionary = {
+  'not-determined': 0,
+  'restricted': 1,
+  'denied': 2,
+  'authorized': 3
+}
 
 export default function App() {
+
+  // GET TRACKING STATUS IF AVALIBLE
+  React.useEffect(() => {
+    getTrackingStatus()
+      .then((status) => {
+        console.log("Current ATT Status at Launch: ",status);
+        MParticle.setATTStatus(dictionary[status])
+      })
+      .catch((e) => Alert.alert('Error', e?.toString?.() ?? e));
+  }, []);
+
+  // PROMPT ATT STATUS
+  const request = React.useCallback(async () => {
+    try {
+      const status = await requestTrackingPermission();
+      console.log("Revised ATT Status: ",status);
+      switch (status) {
+        case 'authorized':
+          MParticle.setATTStatus(dictionary[status])
+          let user = await MParticle.Identity.getCurrentUser((user)=>user.getUserIdentities((ids)=>{
+            if(!Object.keys(ids).includes(MParticle.UserIdentityType.IOSAdvertiserId)){
+              var request = new MParticle.IdentityRequest();
+              request.setUserIdentity(
+                '68c7c373-e0fa-4420-b322-81d5db5b9b24', // DEVICE IDFV
+                MParticle.UserIdentityType.IOSAdvertiserId
+              );
+              MParticle.Identity.modify(request, (error, userId) => { error ? console.debug(error) : null})
+            }
+          }));
+          break;
+        case 'restricted':
+          MParticle.setATTStatus(dictionary[status])
+          break;
+        case 'denied':
+          MParticle.setATTStatus(dictionary[status])
+          break;
+        case 'authorized':
+          MParticle.setATTStatus(dictionary[status])
+          break;
+        default:
+          console.log("Something is wack with this status: ",status)
+      }
+    } catch (e) {
+      Alert.alert('Error', e?.toString?.() ?? e);
+    }
+  }, []);
+  request()
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Trigger Events!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={()=>{
+          MParticle.logEvent(
+            'Button Pressed',
+            MParticle.EventType.Other,
+            { 'Test key': 'Test value' },
+          )}}
+        title='Push Me'
+        accessibilityLabel='Trigger a Custom Event to mParticle'
+      />
     </View>
   );
 }
--- ios/kfcrntest/AppDelegate.m
+++ ios/kfcrntest/AppDelegate.m
@@ -6,6 +6,13 @@
 #import <React/RCTLinkingManager.h>
 #import <React/RCTConvert.h>
 
+// Import mParticle
+#import <mParticle-Apple-SDK/mParticle.h>
+
+// Add Add Tracking Dependencies
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
+#import <AdSupport/AdSupport.h>
+
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -32,15 +39,49 @@ static void InitializeFlipper(UIApplication *application) {
 #if defined(FB_SONARKIT_ENABLED) && __has_include(<FlipperKit/FlipperClient.h>)
   InitializeFlipper(application);
 #endif
-  
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"main" initialProperties:nil];
   id rootViewBackgroundColor = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"RCTRootViewBackgroundColor"];
   if (rootViewBackgroundColor != nil) {
     rootView.backgroundColor = [RCTConvert UIColor:rootViewBackgroundColor];
   } else {
-    rootView.backgroundColor = [UIColor whiteColor];
+//    rootView.backgroundColor = [UIColor whiteColor];
   }
+
+  // Build mParticle Config 
+  MParticleOptions *options = [MParticleOptions optionsWithKey:@"us1-676663aaaca5634cb331a2ce9757bc24"
+                                                        secret:@"f6-XjBN4rSmOfJAefKZmoz8EYAjE9FFldY7F4BEpNq59caNQtg9a6gPC4yFGfol3"];
+  MPIdentityApiRequest *identityRequest = [MPIdentityApiRequest requestWithEmptyUser];
+  options.identifyRequest = identityRequest;
+  options.environment = MPEnvironmentDevelopment;
+  options.logLevel = MPILogLevelVerbose;
+  id identityCallback = ^(MPIdentityApiResult *_Nullable apiResult, NSError *_Nullable error){
+    if (apiResult){
+      //ADD ANY SUCCESSFUL IMPLEMENTATION LOGIC
+    } else {
+      NSLog(@"%@",error.userInfo);
+      switch (error.code) {
+          case MPIdentityErrorResponseCodeClientNoConnection:
+          case MPIdentityErrorResponseCodeClientSideTimeout:
+              //retry the IDSync request
+              break;
+          case MPIdentityErrorResponseCodeRequestInProgress:
+          case MPIdentityErrorResponseCodeRetry:
+              //inspect your implementation if this occurs frequency
+              //otherwise retry the IDSync request
+              break;
+          default:
+              // inspect error.userInfo to determine why the request failed
+              // this typically means an implementation issue
+              break;
+      }
+    }
+  };
+  options.onIdentifyComplete = identityCallback;
+
+  // Initialize mParticle with Config
+  [[MParticle sharedInstance] startWithOptions:options];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
